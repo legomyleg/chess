60f323293eeedef863144b98a5ed23045c8795fb
legomyleg <pearsonroymorris@gmail.com>
2026-02-28 22:10:18 -0700

    Added AuthDAO outline

---
+++ server/src/main/java/dataaccess/AuthDOA.java
@@ -0,0 +1,25 @@
+package dataaccess;
+
+import model.AuthData;
+
+public class AuthDOA {
+    public void createAuth(AuthData authData) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public AuthData getAuthByToken(String authToken) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public AuthData getAuthByUsername(String username) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void deleteAuthByToken(String authToken) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void deleteAuthByUsername(String username) {
+        throw new RuntimeException("Not implemented");
+    }
+}
