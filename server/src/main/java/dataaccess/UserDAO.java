4af0ee9946633e739356a7cf4912c7ecc97c5afd
legomyleg <pearsonroymorris@gmail.com>
2026-02-28 21:58:28 -0700

    Added UserDAO outline

---
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -0,0 +1,21 @@
+package dataaccess;
+
+import model.UserData;
+
+public class UserDAO {
+    public void createUser(UserData user) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public UserData getUser(String username) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void updateUser(String username, UserData newUserData) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void deleteUser(String username) {
+        throw new RuntimeException("Not implemented");
+    }
+}
