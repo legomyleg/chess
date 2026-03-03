9abb7a9a6362721007f16a8d6a224b3224e7cf30
legomyleg <pearsonroymorris@gmail.com>
2026-03-02 19:50:51 -0700

    Added LogoutServiceTest

--- server/src/test/java/service/LoginServiceTest.java
+++ server/src/test/java/service/LoginServiceTest.java
@@ -4,26 +4,31 @@ import dataaccess.AuthDAO;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryUserDAO;
 import dataaccess.UserDAO;
+import exception.IncorrectPasswordException;
 import exception.ResponseException;
 import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import request.LoginRequest;
 import request.RegisterRequest;
 
 class LoginServiceTest {
     AuthDAO authDAO = new MemoryAuthDAO();
     UserDAO userDAO = new MemoryUserDAO();
+    String username = "pboi";
+    String password = "pboi1";
+    String email = "asd@gmail.com";
+    LoginService loginService = null;
 
     @BeforeEach
     void setUp() throws ResponseException {
-        String username = "pboi";
-        String password = "pboi1";
-        String email = "asd@gmail.com";
-
         var registerRequest = new RegisterRequest(username, password, email);
         String authToken = new RegisterService(authDAO, userDAO).register(registerRequest).authToken();
 
         new LogoutService(authDAO).logout(authToken);
+
+        loginService = new LoginService(userDAO, authDAO);
     }
 
     @AfterEach
@@ -35,7 +40,19 @@ class LoginServiceTest {
     @Test
     void loginSuccess() {
         try {
-
+            var request = new LoginRequest(username, password);
+            loginService.login(request);
+        } catch (ResponseException e) {
+            throw new AssertionError("Login should not throw error.");
         }
+    }
+
+    @Test
+    void loginFails() {
+        String wrongPassword = "wrong";
+        var badRequest = new LoginRequest(username, wrongPassword);
+
+        assertThrowsExactly(IncorrectPasswordException.class, () -> loginService.login(badRequest));
+
     }
 }
