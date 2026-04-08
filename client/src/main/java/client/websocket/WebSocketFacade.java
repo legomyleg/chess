b1f0feeecfdde68a1a77f7b957fc7332a0b1cf5a
legomyleg <pearsonroymorris@gmail.com>
2026-04-08 15:47:18 -0600

    Starting on getting the WS stuff complete

---
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -0,0 +1,48 @@
+package client.websocket;
+
+import chess.serialization.GsonFactory;
+import com.google.gson.Gson;
+import exception.ResponseException;
+import jakarta.websocket.*;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebSocketFacade extends Endpoint {
+
+    private Session session;
+    private ServerMessageHandler serverMessageHandler;
+
+    public WebSocketFacade(String url, ServerMessageHandler serverMessageHandler) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
+                ServerMessage serverMessage = GsonFactory.create().fromJson(message, ServerMessage.class);
+                serverMessageHandler.handleMessage(serverMessage);
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    public void connect(String authToken, int gameID) throws ResponseException {
+        try {
+            var command = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (IOException e) {
+            throw new ResponseException(500, e.getMessage());
+        }
+    }
+}
---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,12 @@
+package websocket.messages;
+
+import chess.ChessGame;
+
+public class LoadGameMessage extends ServerMessage {
+    private final ChessGame game;
+
+    public LoadGameMessage(ChessGame game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+}
