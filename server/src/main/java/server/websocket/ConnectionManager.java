5326725bf81e96b905d54ff7e0f57761cf820eaf
legomyleg <pearsonroymorris@gmail.com>
2026-04-08 17:00:10 -0600

    Added more methods to ConnectionManager

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -4,22 +4,23 @@ import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.NotificationMessage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
     public final ConcurrentHashMap<Integer, List<Session>> connections = new ConcurrentHashMap<>();
 
-    public void addGame(Integer gameID, Session session) {
-        connections.;
+    public void addGame(Integer gameID) {
+        connections.put(gameID, new ArrayList<>());
     }
 
     public void addSessionToGame(Integer gameID, Session session) {
-
+        connections.get(gameID).add(session);
     }
 
-    public void remove(Session session) {
-        connections.remove(session);
+    public void removeSessionFromGame(Integer gameID, Session session) {
+        connections.get(gameID).remove(session);
     }
 
     public void broadcast(Session excludeSession, NotificationMessage notification) throws IOException {
