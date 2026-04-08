2fc14143910ff1db89af983a5fdf48b8c3cf428e
legomyleg <pearsonroymorris@gmail.com>
2026-04-08 16:58:00 -0600

    WOrking on ConnectionManager

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,5 +1,33 @@
 package server.websocket;
 
-public class ConnectionManager {
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.NotificationMessage;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<Integer, List<Session>> connections = new ConcurrentHashMap<>();
+
+    public void addGame(Integer gameID, Session session) {
+        connections.;
+    }
+
+    public void addSessionToGame(Integer gameID, Session session) {
+
+    }
+
+    public void remove(Session session) {
+        connections.remove(session);
+    }
+
+    public void broadcast(Session excludeSession, NotificationMessage notification) throws IOException {
+        String msg = notification.toString();
+        for (Session s : connections.values()) {
+            if (s.isOpen() && !s.equals(excludeSession)) {
+                s.getRemote().sendString(msg);
+            }
+        }
+    }
 }
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,5 +1,48 @@
 package server.websocket;
 
-public class WebSocketHandler {
+import chess.serialization.GsonFactory;
+import dataaccess.SQLAuthDAO;
+import dataaccess.SQLGameDAO;
+import io.javalin.websocket.*;
+import org.jetbrains.annotations.NotNull;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.UserGameCommand;
 
+public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+    private SQLGameDAO gameDAO;
+    private SQLAuthDAO authDAO;
+
+    public WebSocketHandler()
+
+    @Override
+    public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
+
+    }
+
+    @Override
+    public void handleConnect(@NotNull WsConnectContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void handleMessage(@NotNull WsMessageContext ctx) throws Exception {
+        String json = ctx.message();
+        UserGameCommand base = GsonFactory.create().fromJson(json, UserGameCommand.class);
+
+        switch (base.getCommandType()) {
+            case CONNECT -> connectToGame(ctx, base);
+            case LEAVE -> leaveGame()
+
+        }
+    }
+
+    private void connectToGame(WsMessageContext ctx, UserGameCommand command) {
+        assert command.getCommandType() == UserGameCommand.CommandType.CONNECT;
+
+        String authToken = command.getAuthToken();
+        Integer gameID = command.getGameID();
+        var game = getGameByGameID
+    }
 }
